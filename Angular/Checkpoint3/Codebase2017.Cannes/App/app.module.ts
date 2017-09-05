570493742fc25a76e3db62f352ae58e2d54f6e7c
andy <andy@ekdahls.net>
2017-09-05 15:05:02 +0200

    Added checkpoint3

---
+++ Angular/Checkpoint3/Codebase2017.Cannes/App/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
